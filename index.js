e51f7046a3a4f0c994a2f1997b6b3304094a3314
KorbinH <99376076+KorbinH@users.noreply.github.com>
2022-02-09 22:05:21 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-setTimeout(() => { window.location.replace("https://www.youtube.com/watch?v=GJuXYdyWP9o");; }, 2000);
+setTimeout(() => { window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");; }, 2000);
 console.log("Enjoy!")
