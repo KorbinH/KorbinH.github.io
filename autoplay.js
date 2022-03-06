48ed86e355caa1cb821f07c1310583d64a4f948c
KorbinH <99376076+KorbinH@users.noreply.github.com>
2022-03-05 21:36:34 -0800

    Delete autoplay.js

--- autoplay.js
+++
@@ -1,7 +0,0 @@
-var audio = document.getElementById("firefox is dumb");
-audio.play();
-setTimeout(() => {document.getElementById('invisible').innerHTML += "<a href='http://wilsonstudios.net'>go to wilsonstudios.net</a><br />";}, 7500)
-setTimeout(() => {document.getElementById('invisible').innerHTML += "<a href='http://wilsonstudios.net'>go to wilsonstudios.net</a><br />";}, 9500)
-setTimeout(() => {document.getElementById('invisible').innerHTML += "<a href='http://wilsonstudios.net'>go to wilsonstudios.net</a><br />";}, 11500)
-setTimeout(() => {document.getElementById('invisible').innerHTML += "<a href='http://wilsonstudios.net'>go to wilsonstudios.net</a><br />";}, 14250)
-setTimeout(() => {document.getElementById('invisible').innerHTML += "<a href='http://wilsonstudios.net'>go to wilsonstudios.net</a><br />";}, 14500)
