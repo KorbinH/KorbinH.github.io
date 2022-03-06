30e09f84269e3e9bcf2d9be1535b98683e98f9d6
KorbinH <99376076+KorbinH@users.noreply.github.com>
2022-03-06 11:17:17 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function water() {
+    window.location.href = "https://hotanimegirls-uwu.com/drink%20that%20water";
+}
+
+function knees() {
+    window.location.href = "https://hotanimegirls-uwu.com/knees/";
+}
