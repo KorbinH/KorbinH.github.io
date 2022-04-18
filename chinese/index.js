56a3d1313d5249509e080fbeacc5ce8e24458616
KorbinH <99376076+KorbinH@users.noreply.github.com>
2022-04-17 20:49:03 -0700

    Add files via upload

---
+++ chinese/index.js
@@ -0,0 +1,12 @@
+function water() {
+    window.location.href = "https://hotanimegirls-uwu.com/drink%20that%20water";
+}
+
+function knees() {
+    window.location.href = "https://hotanimegirls-uwu.com/knees/";
+}
+
+// when you are done makign games
+function games() {
+    window.location.href = ""
+}
